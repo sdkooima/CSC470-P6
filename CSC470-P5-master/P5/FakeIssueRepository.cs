1a99f543de9301f34ddd687e3f1af1a2aa7c73e6
IocusScelestus <36017753+IocusScelestus@users.noreply.github.com>
2020-11-04 19:18:23 -0600

    Created new classes and interfaces as specified by the UML designs

---
+++ CSC470-P5-master/P5/FakeIssueRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5
+{
+    public class FakeIssueRepository : IIssueRepository
+    {
+        public const string NO_ERROR = "";
+        public const string EMPTY_TITLE_ERROR = "A Title is required.";
+        public const string EMPTY_DISCOVERY_DATETIME_ERROR = "Must select a Discovery Date/Time.";
+        public const string FUTURE_DISCOVERY_DATETIME_ERROR = "Issues can't be from the future.";
+        public const string EMPTY_DISCOVERER_ERROR = "A Discoverer is required.";
+        
+        private static List<Issue> _Issues = new List<Issue>();
+
+        private string ValidateIssue(Issue issue)
+        {
+            
+        }
+
+        private bool IsDuplicate(string title)
+        {
+
+        }
+    }
+}
